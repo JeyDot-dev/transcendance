b1d4d7363182cfe411e6b2d473246e4ff8e8ca02
Leo Sohler <lsohler@c1r3s7.42lausanne.ch>
2024-05-20 12:32:47 +0200

    first commit, playing with my doodoo

---
+++ pong/routing.py
@@ -0,0 +1,8 @@
+# pong/routing.py
+
+from django.urls import path
+from pong.consumers import PongConsumer
+
+websocket_urlpatterns = [
+    path('ws/pong/', PongConsumer.as_asgi()),
+]
---
+++ pong/views.py
@@ -0,0 +1,5 @@
+# pong/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'pong/index.html')
